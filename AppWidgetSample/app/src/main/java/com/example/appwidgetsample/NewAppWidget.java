8774760d5e3f857c8c5e1e08b38843b45e706d07
liliancai <yushunchn@gmail.com>
2019-07-16 10:18:26 +0200

    last updated%1 %2

--- AppWidgetSample/app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
+++ AppWidgetSample/app/src/main/java/com/example/appwidgetsample/NewAppWidget.java
@@ -3,20 +3,46 @@ package com.example.appwidgetsample;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
-public class NewAppWidget extends AppWidgetProvider {
+public class NewAppWidget<count> extends AppWidgetProvider {
+
+
+    private static final String mSharedPreFile = "com.example.android.appwidgetsample";
+    private static final String COUNT_KEY = "count";
+
+
+
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        SharedPreferences prefs = context.getSharedPreferences(mSharedPreFile, 0);
+        int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
+        count++;
+
+        String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+
+
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+        SharedPreferences.Editor preEditor = prefs.edit();
+        preEditor.putInt(COUNT_KEY + appWidgetId, count);
+        preEditor.apply();
+
         views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
 
+        views.setTextViewText(R.id.appwidget_update, context.getResources().getString(
+                R.string.date_count_format, count, dateString));
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
