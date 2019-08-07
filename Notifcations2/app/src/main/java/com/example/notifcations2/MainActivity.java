439bf3495459d43469b68e5e72e9a7413e8b76a9
liliancai <yushunchn@gmail.com>
2019-08-07 03:23:53 +0200

    notifcation basic&expandble, bitmap get

--- Notifcations2/app/src/main/java/com/example/notifcations2/MainActivity.java
+++ Notifcations2/app/src/main/java/com/example/notifcations2/MainActivity.java
@@ -5,7 +5,10 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.RemoteInput;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -17,20 +20,88 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
 
-    private int notificationId = 1 ;
+    private static final String KEY_TEXT_REPLY ="" ;
+    private int notificationId = 1;
     private NotificationManager notificationManager;
+    private Bitmap bitmapChihuahuaLookMe;
+    private Object emailObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-    
+
     }
 
     public void sendNotificationBasic(View view) {
 
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://developer.android.com/reference/android/app/Notification.html"));
+      //  Intent snoozeIntent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        //The difference
+        NotificationChannel a;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            a = new NotificationChannel("10086", "Notifaction with channelId", NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(a);
+            mBuilder.setChannelId("10086");
+        }
+        mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Let' go")
+                .setContentText("Click here")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setDefaults(Notification.DEFAULT_SOUND);
+
+        notificationManager.notify(notificationId, mBuilder.build());
+    }
+
+    /**
+     * @param view TODO
+     */
+
+   /* public void sendNotificationMessageReply(View view) {
+
+        String replyLabel = getResources().getString(R.string.reply_label);
+        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
+                .setLabel(replyLabel)
+                .build();
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        PendingIntent.getBroadcast(getApplicationContext(),
+                conversation.getConversationId(),
+                getMessageReplyIntent(conversation.getConversationId()),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(R.drawable.ic_reply_black_24dp,
+                        getString(R.string.reply_label), replyPendingIntent)
+                        .addRemoteInput(remoteInput)
+                        .build();
+        //The difference
+        NotificationChannel a;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            a = new NotificationChannel("10086","Notifaction with channelId", NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(a);
+            mBuilder.setChannelId("10086");
+        }
+        mBuilder.setSmallIcon(R.drawable.ic_favorite_border_black_24dp)
+                .setContentTitle("Let' go")
+                .setContentText("Click here")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setAction(action);
+
+        notificationManager.notify(notificationId, mBuilder.build());
+
+    }*/
+
+    public void sendNotificationExpandable(View view) {
+
+        bitmapChihuahuaLookMe = BitmapFactory.decodeResource(getResources(),R.drawable.chihuahua_lookme);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
         Intent intent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("http://developer.android.com/reference/android/app/Notification.html"));
@@ -43,68 +114,17 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(a);
             mBuilder.setChannelId("10086");
         }
-        mBuilder.setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Let' go")
-                .setContentText("Click here")
+        mBuilder.setSmallIcon(R.drawable.ic_favorite_border_black_24dp)
+                .setContentTitle("Look at me")
+                .setContentText("Am I adorable?")
+                .setLargeIcon(bitmapChihuahuaLookMe)
+               /* .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(bitmapChihuahuaLookMe)
+                        .bigLargeIcon(null))*/
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setDefaults(Notification.DEFAULT_SOUND);
 
-// notificationId is a unique int for each notification that you must define
         notificationManager.notify(notificationId, mBuilder.build());
 
     }
-//    @TargetApi(Build.VERSION_CODES.O)
-//    private void createNotificationChannel() {
-//        // Create the NotificationChannel, but only on API 26+ because
-//        // the NotificationChannel class is new and not in the support library
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            CharSequence channelName = "Application_name";
-//            String channelDescription = "Application_name Alert";
-//           // CharSequence name = getString(R.string.channel_name);
-//          //  String description = getString(R.string.channel_description);
-//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-//            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
-//            channel.setDescription(channelDescription);
-//            // Register the channel with the system; you can't change the importance
-//            // or other notification behaviors after this
-//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-//            notificationManager.createNotificationChannel(channel);
-//        }
-//    }
 }
-/*   public static String createNotificationChannel(Context context) {
-
-        // NotificationChannels are required for Notifications on O (API 26) and above.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            // The id of the channel.
-            String channelId = "Channel_id";
-
-            // The user-visible name of the channel.
-
-            // The user-visible description of the channel.
-
-            int channelImportance = NotificationManager.IMPORTANCE_DEFAULT;
-            boolean channelEnableVibrate = true;
-            //            int channelLockscreenVisibility = Notification.;
-
-            // Initializes NotificationChannel.
-            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, channelImportance);
-            notificationChannel.setDescription(channelDescription);
-            notificationChannel.enableVibration(channelEnableVibrate);
-            //            notificationChannel.setLockscreenVisibility(channelLockscreenVisibility);
-
-            // Adds NotificationChannel to system. Attempting to create an existing notification
-            // channel with its original values performs no operation, so it's safe to perform the
-            // below sequence.
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            assert notificationManager != null;
-            notificationManager.createNotificationChannel(notificationChannel);
-
-            return channelId;
-        } else {
-            // Returns null for pre-O (26) devices.
-            return null;
-        }
-    }*/
-
