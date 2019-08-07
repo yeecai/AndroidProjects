66993f1e6d53da704986a1df444c74d360b2828e
liliancai <yushunchn@gmail.com>
2019-08-07 02:14:11 +0200

    channel Id get

--- Notifcations2/app/src/main/java/com/example/notifcations2/MainActivity.java
+++ Notifcations2/app/src/main/java/com/example/notifcations2/MainActivity.java
@@ -1,54 +1,110 @@
 package com.example.notifcations2;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String CHANNEL_ID = ;
+
     private int notificationId = 1 ;
+    private NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        Intent intent = new Intent(this, AlertDetails.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Hmm")
-                .setContentText("Hmmm")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-// notificationId is a unique int for each notification that you must define
-        notificationManager.notify(notificationId, builder.build());
     
     }
 
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+    public void sendNotificationBasic(View view) {
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://developer.android.com/reference/android/app/Notification.html"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        //The difference
+        NotificationChannel a;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            a = new NotificationChannel("10086","Notifaction with channelId", NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(a);
+            mBuilder.setChannelId("10086");
         }
+        mBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Let' go")
+                .setContentText("Click here")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setDefaults(Notification.DEFAULT_SOUND);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, mBuilder.build());
+
     }
+//    @TargetApi(Build.VERSION_CODES.O)
+//    private void createNotificationChannel() {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence channelName = "Application_name";
+//            String channelDescription = "Application_name Alert";
+//           // CharSequence name = getString(R.string.channel_name);
+//          //  String description = getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+//            channel.setDescription(channelDescription);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
 }
+/*   public static String createNotificationChannel(Context context) {
+
+        // NotificationChannels are required for Notifications on O (API 26) and above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            // The id of the channel.
+            String channelId = "Channel_id";
+
+            // The user-visible name of the channel.
+
+            // The user-visible description of the channel.
+
+            int channelImportance = NotificationManager.IMPORTANCE_DEFAULT;
+            boolean channelEnableVibrate = true;
+            //            int channelLockscreenVisibility = Notification.;
+
+            // Initializes NotificationChannel.
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, channelImportance);
+            notificationChannel.setDescription(channelDescription);
+            notificationChannel.enableVibration(channelEnableVibrate);
+            //            notificationChannel.setLockscreenVisibility(channelLockscreenVisibility);
+
+            // Adds NotificationChannel to system. Attempting to create an existing notification
+            // channel with its original values performs no operation, so it's safe to perform the
+            // below sequence.
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            return channelId;
+        } else {
+            // Returns null for pre-O (26) devices.
+            return null;
+        }
+    }*/
+
--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Collection of any study material related to AD
 1.
 
 2.
+
+3.
+
+-Notifcations, notfication before API 26 without channelID
+
+-Notifactions2, basic notfication usage API>=26
+
