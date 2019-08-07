726afc566ab9893c2c1b7298cd692ff4be139b15
liliancai <yushunchn@gmail.com>
2019-08-07 13:29:29 +0200

    butterknife get

--- OkHttp01/app/src/main/java/com/example/okhttp01/MainActivity.java
+++ OkHttp01/app/src/main/java/com/example/okhttp01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.okhttp01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +19,7 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewResult;
+    private String TAG = "hmm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         OkHttpClient client = new OkHttpClient();
 
-        String url = "http://120.78.214.127/";
+      //  String url = "http://120.78.214.127";
+        String url = "https://www.baidu.com";
+        //String url = "https://reqres.in/api/users?page=2";
 
         Request request = new Request.Builder()
                 .url(url)
@@ -44,14 +48,34 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 if(response.isSuccessful()) {
                     final String myResponse = response.body().string();
-                    
+                   // Log.d(TAG, myResponse);
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             mTextViewResult.setText(myResponse);
                         }
                     });
-                }
+                }else Log.d(TAG,"failed");
+            }
+        });
+    }
+
+    private void doGet() {
+        //1.获取OkHttpClient对象
+        OkHttpClient okHttpClient = new OkHttpClient();
+        //2.获取Request对象
+        Request request = new Request.Builder().get().url("http://www.baidu.com").build();
+        //3.将Request封装为Call对象
+        Call call = okHttpClient.newCall(request);
+        //4.执行Call
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: " + e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.e(TAG, "onResponse: " + response.body().string());
             }
         });
     }
---
+++ testButterknife/app/src/main/java/com/example/testbutterknife/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testbutterknife;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.textView)
+    public void test() {
+        textView.setText("okidoki");
+    }
+}
